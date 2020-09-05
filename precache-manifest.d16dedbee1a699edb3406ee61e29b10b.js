506171c37a450bafecb8da8ab90b6a20860729ad
alonlerner <al663@njit.edu>
2020-09-05 12:49:50 -0400

    Updates

---
+++ precache-manifest.d16dedbee1a699edb3406ee61e29b10b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb35cae5fdf7b6cc92c040d0aa70449",
+    "url": "/njit-trivia/index.html"
+  },
+  {
+    "revision": "74a6ba6a8c8faa332da9",
+    "url": "/njit-trivia/static/css/main.2a85100b.chunk.css"
+  },
+  {
+    "revision": "d3da6def73f586fff56d",
+    "url": "/njit-trivia/static/js/2.2df6306e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/njit-trivia/static/js/2.2df6306e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a6ba6a8c8faa332da9",
+    "url": "/njit-trivia/static/js/main.335573c7.chunk.js"
+  },
+  {
+    "revision": "d11fbe6c7aca7dbe159e",
+    "url": "/njit-trivia/static/js/runtime-main.2222244e.js"
+  },
+  {
+    "revision": "2bc746fd3996bf4353da5aac08d0c8ba",
+    "url": "/njit-trivia/static/media/heart2.2bc746fd.png"
+  }
+]);
